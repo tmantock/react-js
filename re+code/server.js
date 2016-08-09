a69ec0a7fd13a6d92f81b87a2e9eee2ebfa59db0
Tevin Mantock <tmantock@gmail.com>
2016-08-08 21:42:51 -0700

    port assigned for node server

---
+++ re+code/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,function () {
+  console.log(`Listening on PORT: ${PORT}`);
+})
