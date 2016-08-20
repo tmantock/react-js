939dd989e814e625afb4605aa3578f2900327daf
Tevin Mantock <tmantock@gmail.com>
2016-08-19 17:45:08 -0700

    authenticated higher order component

--- higher-order/src/index.js
+++ higher-order/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
-
+import requireAuth from './components/require-auth';
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="/resources" component={Resources} />
+        <Route path="/resources" component={requireAuth(Resources)} />
       </Route>
     </Router>
   </Provider>
