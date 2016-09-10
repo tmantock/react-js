9b7081b8d1b09c310ea16c2bf2a1f2bb237d134b
Tevin Mantock <tmantock@gmail.com>
2016-09-09 20:36:44 -0700

    package update

--- authentication/server/server.js
+++ authentication/server/server.js
@@ -5,7 +5,10 @@ const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
-
+//Middleware
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+//Starting the server
 server.listen(PORT, function(){
   console.log(`Listening on PORT: 3000`);
 })
