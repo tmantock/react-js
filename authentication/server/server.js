7cc034c2962fd84f5aba3b3f51cde9135e013e2f
Tevin Mantock <tmantock@gmail.com>
2016-09-09 21:07:07 -0700

    mongodb setup

--- authentication/server/server.js
+++ authentication/server/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser')
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const router = require('./router');
+//DB setup
+mongoose.connect(config.location);
 //Middleware
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
