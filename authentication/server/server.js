657abb74aa286d56568b912e6cd7bfd6dc06dd37
Tevin Mantock <tmantock@gmail.com>
2016-09-09 20:44:41 -0700

    router applied

--- authentication/server/server.js
+++ authentication/server/server.js
@@ -5,10 +5,14 @@ const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
+const router = require('./router');
 //Middleware
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+
+
 //Starting the server
+router(app);
 server.listen(PORT, function(){
   console.log(`Listening on PORT: 3000`);
 })
