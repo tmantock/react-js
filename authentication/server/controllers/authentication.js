2b9c34da7916c6b4e98380bd24e225b352250de3
Tevin Mantock <tmantock@gmail.com>
2016-09-12 19:30:24 -0700

    js web tokens

--- authentication/server/controllers/authentication.js
+++ authentication/server/controllers/authentication.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
@@ -25,8 +32,8 @@ exports.signup = function (req, res, next) {
           return next(err);
         }
 
-        res.json({ success: true });
+        res.json({ success: true, token: tokenForUser(user) });
       });
-  })
+  });
 
 }
