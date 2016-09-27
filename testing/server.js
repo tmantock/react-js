d4a48f6fb8e2b07b777fe5fa94c82704ed931934
Tevin Mantock <tmantock@gmail.com>
2016-09-27 10:14:37 -0700

    initializing test repo for react and node

---
+++ testing/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res, next) {
+  res.send({'response': 'Hello Mate!'});
+  next();
+})
+
+
+
+app.listen( PORT, function () {
+  console.log(`Listening on port: ${PORT}`);
+})
