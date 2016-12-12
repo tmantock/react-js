d7da0cbacf800c16a587f9515f3c698216196beb
Tevin Mantock <tmantock@gmail.com>
2016-12-11 21:13:44 -0800

    solving cors error

--- authentication/client/src/actions/index.js
+++ authentication/client/src/actions/index.js
@@ -4,8 +4,8 @@ const API_URL = 'http://localhost:3000';
 
 export function signinUser ({ email, password}){
     return function(dispatch) {
-        axios.post(`${API_URL}/sigin`, { email, password });
-        
+        axios.post(`${API_URL}/signin`, { email, password });
+
     }
     //Submit credentials to server
 
--- authentication/server/server.js
+++ authentication/server/server.js
@@ -8,10 +8,12 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const router = require('./router');
+const cors = require('cors');
 //DB setup
 mongoose.connect(config.location);
 //Middleware
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
