7c5ee7e80060f57e7735e9af6e1b1aa30838ed56
Tevin Mantock <tmantock@gmail.com>
2016-12-14 21:59:05 -0800

    router push

--- authentication/client/src/actions/index.js
+++ authentication/client/src/actions/index.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const API_URL = 'http://localhost:3000';
 
 export function signinUser ({ email, password}){
     return function(dispatch) {
-        axios.post(`${API_URL}/signin`, { email, password });
+        axios.post(`${API_URL}/signin`, { email, password })
+            .then(response => {
+                browserHistory.push('/feature');
+            })
+            .catch(() => {
+
+            });
 
     }
-    //Submit credentials to server
-
-    //If request is good
-
-    //If request is bad
 }
