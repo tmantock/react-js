3bed6060f7446774d121cca5ba339906539bd44d
Tevin Mantock <tmantock@gmail.com>
2016-12-18 18:18:24 -0800

    field validation with redux form

--- authentication/client/src/components/auth/signup.js
+++ authentication/client/src/components/auth/signup.js
@@ -3,13 +3,20 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class SignUp extends Component {
+    handleFormSubmit(formProps){
+        //Call action creator to sign up the user
+        this.props.signUpUser(formProps);
+    }
+
     render(){
-        const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+        const { handleSubmit, pristine, reset, submitting, fields: { email, password, passwordConfirm } } = this.props;
+
         return (
-            <div>
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className = "form-group">
                     <label>Email:</label>
                     <input {...email} className = "form-control" />
+                    { email.touched && email.error && <div className="error"> {email.error} </div>}
                 </fieldset>
                 <fieldset className = "form-group">
                     <label>Password:</label>
@@ -19,15 +26,28 @@ class SignUp extends Component {
                 <fieldset className = "form-group">
                     <label>Confirm Password:</label>
                     <input {...passwordConfirm} className = "form-control" type="password" />
+                    { passwordConfirm.touched && passwordConfirm.error && <div className="error"> {passwordConfirm.error} </div> }
                 </fieldset>
-                <button className="btn btn-primary">SIgn Up</button>
-            </div>
+                <button className="btn btn-primary" disabled={ pristine || submitting || email.error || password.error || passwordConfirm.error } >Sign Up</button>
+            </form>
         )
     }
 }
 
 function validate(formProps) {
     const errors = {};
+
+    const messages = {
+        email: 'Please enater an email',
+        password: 'Please enter a password',
+        passwordConfirm: 'Please enter a password confirmation'
+    }
+
+    for(var prop in formProps){
+        if(!formProps[prop]){
+            errors[prop] = messages[prop];
+        }
+    }
     
     if(formProps.password !== formProps.passwordConfirm){
         errors.password = 'Passwords must match';
