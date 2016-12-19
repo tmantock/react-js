267223c282e61cfdd163c05acdb9fcb0be852264
Tevin Mantock <tmantock@gmail.com>
2016-12-18 19:13:24 -0800

    authenticated request to server

--- authentication/client/src/actions/index.js
+++ authentication/client/src/actions/index.js
@@ -46,3 +46,14 @@ export function signOutUser(){
     localStorage.removeItem('token');
     return { type: UNAUTH_USER };
 }
+
+export function fetchMessage() {
+    return function(dispatch){
+        axios.get(API_URL, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+            .then(response => {
+                console.log(response);
+            })
+    }
+}
--- authentication/server/router.js
+++ authentication/server/router.js
@@ -7,7 +7,10 @@ const requireSignin = passport.authenticate('local', {session: false });
 
 module.exports = function (app) {
   app.get('/', requireAuth, function (req, res) {
-    res.send({ success: true });
+    res.send({ 
+      success: true,
+      message: "You've reached the secret route"
+     });
   });
 
   app.post('/signup', Authentication.signup);
