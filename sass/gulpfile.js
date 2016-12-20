cba8ee9639dc159b3856dc10474e4474cff974a3
Tevin Mantock <tmantock@gmail.com>
2016-12-19 22:22:20 -0800

    gulp file to only compile mains sass file and imports

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-    gulp.src('./sass/*.scss')
+    gulp.src('./sass/main.scss')
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
